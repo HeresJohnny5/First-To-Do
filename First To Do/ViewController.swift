6f1cc621122ea0fc3263898d9a0e81e775d74f8e
HeresJohnny5 <ericksonjohn01@hotmail.com>
2018-12-03 19:39:57 -0500

    Initial Commit

---
+++ First To Do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First To Do
+//
+//  Created by John Erickson on 12/3/18.
+//  Copyright © 2018 John Erickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
